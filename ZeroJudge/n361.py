bc9eee31385a3fdfa2ef61c148df7843d069aa8b
cr1aftooret <cr1aftooret@gmail.com>
2024-04-03 21:51:54 +0800

    n361

---
+++ ZeroJudge/n361.py
@@ -0,0 +1,26 @@
+from math import sqrt, pow
+
+n = int(input())
+
+customer = map(int,input().split())
+
+for i in customer:
+    first = i % 6 == 0
+    second = int(str(i)[-1]) % 2 == 1 and i % 3 != 0
+    third = (pow(int(sqrt(i)), 2) == i) or (i % 7 != 0 and i % 2 == 0)
+
+    # print(first ,second, third)
+
+    if (first and second) or (second and third) or (first and third):
+        if first:
+            print("1", end= " ")
+        else:
+            print("2", end= " ")
+    elif first:
+        print("1", end= " ")
+    elif second:
+        print("2", end= " ")
+    elif third:
+        print("3", end= " ")
+    else:
+        print("0", end= " ")
